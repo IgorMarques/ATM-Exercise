d4ebf0f64da05e4666418f1d17a93b161068683e
Igor Marques da Silva <igormarquessilva@gmail.com>
2015-03-19 15:03:00 -0300

    Add debit operation with @lucasbibiano

---
+++ app/concepts/accounts/business.rb
@@ -0,0 +1,21 @@
+class Account < ActiveRecord::Base
+
+  class Debit < Trailblazer::Operation
+
+    contract do
+      property :account, validates: {presence: true}
+      property :value,  validates: {presence: true}
+    end
+
+    def process(params)
+      validate(params) do
+        account = Account.find(params[:account])
+
+        account.balance -= params[:value]
+
+        account.save
+      end
+    end
+  end
+
+end
