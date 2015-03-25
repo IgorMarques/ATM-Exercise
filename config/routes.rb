82eb52a1e933b522fcb838e9e64404b8c3f895af
Igor Marques da Silva <igormarquessilva@gmail.com>
2015-03-25 16:54:31 -0300

    Remove comments from routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   resources :accounts do
     member do
       post "/process_transaction", to: "accounts#process_transaction", as: :process_transaction
-      # post "/debit", to: "accounts#debit", as: :debit
-      # post "/credit", to: "accounts#credit", as: :credit
     end
   end
 
