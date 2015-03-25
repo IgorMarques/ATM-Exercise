cb0b45acb4ab70129d7ecaf9252f6153dc3c6959
Igor Marques da Silva <igormarquessilva@gmail.com>
2015-03-25 16:54:01 -0300

    Add id to account label with @lucasbibiano

--- app/decorators/account_decorator.rb
+++ app/decorators/account_decorator.rb
@@ -1,6 +1,10 @@
 class AccountDecorator < Draper::Decorator
   delegate_all
 
+  def owner
+    object.id.to_s +  " - " + object.owner
+  end
+
   def balance
     "R$ " + object.balance.to_s.gsub(".", ",")
   end
